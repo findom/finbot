57be00fddbb159a90bf2f1e9b47ae3199b958a6a
findom <findom@users.noreply.github.com>
2017-02-08 12:54:14 -0500

    Added weather API key.

--- behavior/scripts/lib/getCurrentWeather.js
+++ behavior/scripts/lib/getCurrentWeather.js
@@ -3,7 +3,7 @@
 const request = require('request')
 
 module.exports = function getCurrentWeather(weatherAPIKey, locationName, next) {
-  const requestUrl = `http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${weatherAPIKey}&q=${locationName}`
+  const requestUrl = `http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${cd1151ee89fcbfb88ab227df11e28190}&q=${locationName}`
 
   console.log('Making HTTP GET request to:', requestUrl)
 
